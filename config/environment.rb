6266fd0b9297b6fe77cdd000c938cd8aab699db4
Denis Kirichenko <denis.kirichenko@gmail.com>
2013-03-03 20:36:13 +0400

    add perftools profiler

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+Board::Application.configure do
+  config.middleware.use ::Rack::PerftoolsProfiler, :default_printer => 'text', :bundler => true
+end
+
 # Initialize the rails application
 Board::Application.initialize!
